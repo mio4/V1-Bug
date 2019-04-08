57367c3fe5aaab933e604d34745dfc154097af80
Cookize <zy.song@qq.com>
2019-04-09 00:23:17 +0800

    Update LoginController.php

--- bbs/alpha/www/controller/LoginController.php
+++ bbs/alpha/www/controller/LoginController.php
@@ -6,8 +6,22 @@
  * Time: 22:33
  */
 
+require_once '../service/LoginService.php';
+
 class LoginController
 {
-    
+    public static function login()
+    {
+        //从POST体获取数据
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        $isByName = true;
+        // TODO:检查数据是否合法，并分类登陆方式
+
+        //调用Service层逻辑
+        // TODO:选择不同中登陆服务
+        $isSuccess = RegisterService::register_service($username,$password,$email,$type);
+        // TODO:返回合法JSON
+    }
 }
 
