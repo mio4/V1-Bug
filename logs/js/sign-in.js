7ae2f4bd31fb199e7bca5b8a534f24a38f047d40
Zhang <imcuna@gmail.com>
2019-04-09 22:58:36 +0800

    add sign-in and up pages'

---
+++ logs/js/sign-in.js
@@ -0,0 +1,28 @@
+function sign_in(){
+	var data = toJSON();
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("POST","usr/sign_in",true);
+	xmlhttp.setRequestHeader('content-type', 'application/json');
+
+    xmlhttp.onreadystatechange=function(){
+      if (xmlhttp.readyState==4){
+      	if(xmlhttp.getResponseHeader('content-type')==='application/json'){
+	      var result = JSON.parse(xmlhttp.responseText);	
+	  	  if(result.status===400){
+            alert('登录失败');
+	      }
+	      else{
+	      	alert("登录成功");
+	      }
+        }
+        else{
+        	console.log(xmlhttp.responseText);
+        }
+      }
+    }
+	xmlhttp.send(JSON.stringify(data));
+}
+function toJSON(){
+	x=$("form").serializeArray();
+	return x;
+}
