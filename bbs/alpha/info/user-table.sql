05f7f3c303b68c89c25ce9c466a0fcef9cb240b7
Cookize <zy.song@qq.com>
2019-04-12 17:19:20 +0800

    更新数据库建立SQL语句

--- bbs/alpha/info/user-table.sql
+++ bbs/alpha/info/user-table.sql
@@ -1,7 +1,9 @@
-create table `user`(
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user`(
     `uid` int(8) primary key auto_increment,
-    `username` varchar(32) unique not null,
+    `user_name` varchar(32) unique not null,
     `password` varchar(256) not null,
-    `email` varchar(32) not null unique,
-    `type` int(8) not null
+    `user_email` varchar(32) not null unique,
+    `user_kind` int(8) not null,
+    `user_regTime` datetime not null
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
