2e66eadfce5a73f1f45bc1d3d560bed0d7518ee7
Cookize <zy.song@qq.com>
2019-04-13 23:54:18 +0800

    完成登陆注册demo，准备扩展

--- bbs/demo/app/Http/Controllers/UserController.php
+++ bbs/demo/app/Http/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -30,7 +32,44 @@ class UserController extends Controller
 
     public function signUpProcess()
     {
-       echo "OK";
-       exit;
+        $input = request()->all();
+        // var_dump($input);
+
+        $rules = [
+            'nickname' => [
+                'required',
+                'max:50'
+            ],
+            'e-mail' => [
+                'required',
+                'max:50',
+                'email'
+            ],
+            'password' => [
+                'required',
+                'same:password_confirmation',
+                'min:6'
+            ],
+            'password_confirmation' => [
+                'required',
+                'min:6'
+            ],
+            'type' => [
+                'required',
+                'in:G,A'
+            ]
+        ];
+
+        $validator = Validator::make($input, $rules);
+
+        if($validator->fails())
+        {
+            return redirect('usr/sign-up')
+                ->withErrors($validator)        // 带错误信息的重定向
+                ->withInput();                  // 保存原输入
+        }
+
+        $input['password'] = Hash::make($input['password']);    // 使用APP_KEY进行加密
+        exit;
     }
 }
--- bbs/demo/resources/views/sign-up.blade.php
+++ bbs/demo/resources/views/sign-up.blade.php
@@ -7,35 +7,40 @@
     <div class="container">
         <h1>{{ $title }}</h1>
 
-        <form action="/usr/sign-up" method="post">
+        @include('components.validationErrorMessage')
+
+        <form action="sign-up" method="post">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
             <label>
                 昵称：
                 <input type="text"
                        name="nickname"
                        placeholder="昵称"
+                       value="{{ old('nickname') }}"
                 >
             </label>
             <br/>
             <label>
                 Email：
                 <input type="text"
-                       name="nickname"
+                       name="e-mail"
                        placeholder="E-mail"
+                       value="{{ old('e-mail') }}"
                 >
             </label>
             <br/>
             <label>
                 密码：
-                <input type="text"
-                       name="nickname"
+                <input type="password"
+                       name="password"
                        placeholder="密码"
                 >
             </label>
             <br/>
             <label>
                 确认密码：
-                <input type="text"
-                       name="nickname"
+                <input type="password"
+                       name="password_confirmation"
                        placeholder="确认密码"
                 >
             </label>
